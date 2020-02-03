2a5069dd52b27c3e62a671ee9e730cb42af12608
gahsty <30657669+gahsty@users.noreply.github.com>
2020-02-03 18:11:21 -0500

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-package osproj;
-
-public class Main {
-	public static void main(String[] args){
-		process start= new process("C:\\Users\\dell\\Desktop\\test.txt",9);
-		start.runprocess();	
-	}		
-}
