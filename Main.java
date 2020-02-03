bd84b8819c8ffe278b69abc948a66fb860ccb8fa
gahsty <30657669+gahsty@users.noreply.github.com>
2020-02-03 18:17:42 -0500

    Add files via upload
    rr algorithm simulation using java
    Cpu scheduling

---
+++ Main.java
@@ -0,0 +1,8 @@
+package osproj;
+
+public class Main {
+	public static void main(String[] args){
+		process start= new process("C:\\Users\\dell\\Desktop\\test.txt",9);
+		start.runprocess();	
+	}		
+}
